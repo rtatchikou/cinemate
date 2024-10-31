58e999cf76a0368385733a4f358c847b6e151e03
rtatchikou <r.tatchikou@gmail.com>
2024-10-30 23:23:05 -0400

    my cinamate version of the code

---
+++ src/pages/Search.js
@@ -0,0 +1,34 @@
+import { useSearchParams } from "react-router-dom";
+import { Card } from "../components";
+import { useFetch } from "../hooks/useFetch";
+import { useTitle } from "../hooks/useTitle";
+
+import { useEffect } from "react";
+
+export const Search = ({ apiPath }) => {
+  const [searchParams] = useSearchParams();
+  const queryTerm = searchParams.get("q");
+
+  const { data: movies } = useFetch(apiPath, queryTerm);
+
+  const pageTitle = useTitle(`Search result for ${queryTerm}`);
+
+  return (
+    <main>
+      <section>
+        <p className="text-3xl text-gray-700 dark:text-white">
+          {movies.length === 0
+            ? `No results found for '${queryTerm}'`
+            : `Results for '${queryTerm}'`}
+        </p>
+      </section>
+      <section className="max-w-7xl mx-auto py-7">
+        <div className="flex justify-start flex-wrap">
+          {movies.map((movie) => (
+            <Card key={movie.id} movie={movie} />
+          ))}
+        </div>
+      </section>
+    </main>
+  );
+};
