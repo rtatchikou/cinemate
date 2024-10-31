863f6527da34fadb625151cd0dc046d5af470ce2
rtatchikou <r.tatchikou@gmail.com>
2024-10-31 00:30:03 -0400

    fixing unused definition

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,22 +12,38 @@ export const Footer = () => {
       </span>
       <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
-          <a href="#" target="_blank" className="mr-4 hover:underline md:mr-6 ">
+          <a
+            href="https://www.instagram.com/"
+            target="_blank"
+            className="mr-4 hover:underline md:mr-6 "
+          >
             Instagram
           </a>
         </li>
         <li>
-          <a href="#" target="_blank" className="mr-4 hover:underline md:mr-6">
+          <a
+            href="https://www.linkedin.com/"
+            target="_blank"
+            className="mr-4 hover:underline md:mr-6"
+          >
             LinkedIn
           </a>
         </li>
         <li>
-          <a href="#" target="_blank" className="mr-4 hover:underline md:mr-6">
+          <a
+            href="https://www.youtube.com/"
+            target="_blank"
+            className="mr-4 hover:underline md:mr-6"
+          >
             Youtube
           </a>
         </li>
         <li>
-          <a href="#" target="_blank" className="hover:underline">
+          <a
+            href="https://www.github.com/"
+            target="_blank"
+            className="hover:underline"
+          >
             Github
           </a>
         </li>
